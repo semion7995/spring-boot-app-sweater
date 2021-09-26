d77f2753f9b96e105de9194d3333302557c62d04
Semen <semaqwinnert@gmail.com>
2021-09-26 14:58:49 +0300

    add links between database tables (one to many)

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -1,11 +1,15 @@
 package com.example.sweater.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "usr")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -19,6 +23,38 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
     public Long getId() {
         return id;
     }
@@ -31,9 +67,6 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getPassword() {
         return password;
